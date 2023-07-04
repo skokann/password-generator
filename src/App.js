af35134b3c117d646c3c141b602f6ff509f1e5c9
Jiří Zubryckyj <64204363+skokann@users.noreply.github.com>
2023-07-04 23:31:16 +0200

    none

--- src/App.js
+++ src/App.js
@@ -2,31 +2,37 @@ import "./App.css";
 import copy from "./img/copy.svg";
 
 function App() {
-  return (
+  return (  
     <div className="App">
-      <div className="w-full h-screen bg-[#121117]">
-        <div className="container">
-          <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-[#55545C]  text-2xl mb-4">
-              Password Generatorrr
-            </h1>
-            <div className="bg-[#24232B] w-96 h-16 mb-4 flex ">
-              <h1 className="w-full mt-5">henlo</h1>
-              <img className="mr-5" src={copy} alt="copy" />
-            </div>
-            <div className=" w-96 h-96  bg-[#24232B]">
-              <div className="w-full  flex mt-5 ">
-                <h1 className="w-5/6 text-xl pr-28 text-white">
-                  Character Lenght
-                </h1>
-                <p className="w-1/6 text-3xl text-[#A4FFAF]">0</p>
-              </div>
+      <div className="w-full h-full bg-[#121117]">
+        <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="mb-4 text-[#7F7D8B]">Password Generator</h1>
+          <div className="w-96  mb-3 h-16 flex flex-row bg-[#24232B]">
+            <p className="w-5/6 h-full text-[#55545C] mr-44 ">P44/5W0rD!</p>
+            <img className="w-1/6  h-full" src={copy}></img>
+          </div>
 
-              </div>
-            </div>
+          <div className="w-96 h-96 flex flex-col bg-[#24232B]">
+        <div className="flex flex-row mb-5">
+          <p>Character Lenght</p>
+          <p> 8</p>
+          <div>Slider</div>
+        </div>
+        <div className="flex flex-col mb-5">
+          <p>Include Uppercase Letters</p>
+          <p>Include Lowercase Letters</p>
+          <p>Include Numbers</p>
+          <p>Include Symsolb</p>
+        </div>
+        <div className="flex flex-col">
+          <btn>STRENGHT</btn>
+          <btn>Generate</btn>
+
+        </div>
           </div>
         </div>
       </div>
+    </div>
   );
 }
 export default App;
